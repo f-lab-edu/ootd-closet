ce398417ba57bc12a142fdd038e4421ce3302b83
백재우 <9712jw@gmail.com>
2025-08-20 22:04:38 +0900

    Refactor: 패키지 위치 변경에 맞게 수정, 리턴 타입 수정

--- closet-batch/src/main/java/project/closet/batch/processor/WeatherApiItemProcessor.java
+++ closet-batch/src/main/java/project/closet/batch/processor/WeatherApiItemProcessor.java
@@ -5,16 +5,17 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
-import project.closet.batch.api.WeatherAPIClient;
-import project.closet.batch.api.WeatherApiResponseConverter;
-import project.closet.batch.api.response.WeatherApiResponse;
-import project.closet.weather.WeatherData;
-import project.closet.weather.WeatherDataParser;
+import project.closet.api.WeatherAPIClient;
+import project.closet.api.WeatherApiResponseConverter;
+import project.closet.api.response.WeatherApiResponse;
 import project.closet.weather.entity.Weather;
+import project.closet.weather.parser.WeatherData;
+import project.closet.weather.parser.WeatherDataParser;
 import project.closet.weatherlocation.WeatherLocation;
 
 @Component
@@ -43,9 +44,10 @@ public class WeatherApiItemProcessor implements ItemProcessor<WeatherLocation, L
             forecastTime
         );
         // api 응답을 시스템이 가지는 스펙으로 컨버터
-        List<WeatherData> weatherData = weatherApiResponseConverter.parseToWeatherDataList(apiResponse);
+        Map<LocalDate, List<WeatherData>> weatherData =
+            weatherApiResponseConverter.parseToWeatherDataList(apiResponse);
 
         // 도메인 엔티티로 변경 -> 하나의 좌표에 여러 개의 날씨 시간 데이터
-        return weatherDataParser.parseToWeatherEntities(weatherData, forecastedAt);
+        return weatherDataParser.parseToWeatherEntities(weatherData);
     }
 }
